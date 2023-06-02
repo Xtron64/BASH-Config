eb14e9d804534d2ddabed62299c61139fdd4b332
Xtron64 <snekkieboi@pm.me>
2023-06-02 23:33:42 +0100

    Added a BASH config for OpenSUSE

---
+++ opensuse_config/bashrc_opensuse.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Aliases file
+source ~/.config/bash/opensuse_config/aliases_opensuse.sh
+# Path file
+source ~/.config/bash/opensuse_config/path_opensuse.sh
+# Startup file
+source ~/.config/bash/opensuse_config/startup_opensuse.sh
+# Starship BASH config
+source ~/.config/bash/opensuse_config/starship_bash_config_opensuse.sh
+# Zoxide BASH config
+source ~/.config/bash/opensuse_config/zoxide_bash_config_opensuse.sh
